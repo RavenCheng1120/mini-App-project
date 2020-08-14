2b34f78f3995cf3ebde0c44edac5fb447a72bbcf
RavenCheng1120 <ravencheng66@gmail.com>
2020-08-14 16:05:02 +0800

    add animation

---
+++ fluttube/android/app/src/main/java/com/fluttube/MainActivity.java
@@ -0,0 +1,6 @@
+package com.fluttube;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ fluttube/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'splash_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutTube',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false, // 去除右上方Debug標誌
+      home: SplashPage(),
+    );
+  }
+}
